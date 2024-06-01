aa2373514680268d6dc56666e6d27032a11efc1a
Binshad K <binshadcs@gmail.com>
2024-06-01 20:35:11 +0530

    test dratf pr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h2>Green Clean Earth</h2>
+      <h2>Green Clean Earth</h2> // test file 
     </main>
   );
 }
