f9e9ba77f1cd1ce573152eb552e186f8e7b35900
Binshad K <binshadcs@gmail.com>
2024-06-02 23:28:33 +0530

    initial schema generated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h2>Green Clean Earth</h2> // test file 
+      <h2>Green Clean Earth</h2>
     </main>
   );
 }
