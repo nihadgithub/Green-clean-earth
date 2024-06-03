2ee09f3a6a123a20ff69e59d2cd8ff2abfc1261e
Nihad <85496000+nihadgithub@users.noreply.github.com>
2024-06-03 22:25:58 +0530

    ui - my-page table

--- src/app/my-page/activities_tab.tsx
+++ src/app/my-page/activities_tab.tsx
@@ -1,6 +1,67 @@
 import { Button } from "@/components/ui/button";
 import { Upload } from "lucide-react";
 import { DialogUploadActivities } from "./dialog_upload_activities";
+import Table from "@/components/table";
+
+const headings = ['Sl No', 'Views, Likes, Comments, and Shares', 'Category', 'Name and Address of Participant', 'Name of Art - Brief Description', 'Thumbnail', 'Value']
+
+const data = [
+  {
+      id: '1',
+      social: '1000 Views, 200 Likes, 120 Comments and 38 Shares',
+      category: 'Green Activities',
+      participant_details: 'Sherlock Holmes, 221B Baker Street, London',
+      description: 'Youtube video',
+      thumbnail: '..',
+      value: '200'
+  },
+  {
+    id: '2',
+    social: '1000 Views, 200 Likes, 120 Comments and 38 Shares',
+    category: 'Green Activities',
+    participant_details: 'Sherlock Holmes, 221B Baker Street, London',
+    description: 'Youtube video',
+    thumbnail: '..',
+    value: '200'
+  },
+  {
+    id: '3',
+    social: '1000 Views, 200 Likes, 120 Comments and 38 Shares',
+    category: 'Green Activities',
+    participant_details: 'Sherlock Holmes, 221B Baker Street, London',
+    description: 'Youtube video',
+    thumbnail: '..',
+    value: '200'
+  },
+  {
+    id: '4',
+    social: '1000 Views, 200 Likes, 120 Comments and 38 Shares',
+    category: 'Green Activities',
+    participant_details: 'Sherlock Holmes, 221B Baker Street, London',
+    description: 'Youtube video',
+    thumbnail: '..',
+    value: '200'
+  },
+  {
+    id: '5',
+    social: '1000 Views, 200 Likes, 120 Comments and 38 Shares',
+    category: 'Green Activities',
+    participant_details: 'Sherlock Holmes, 221B Baker Street, London',
+    description: 'Youtube video',
+    thumbnail: '..',
+    value: '200'
+  },
+  {
+    id: '6',
+    social: '1000 Views, 200 Likes, 120 Comments and 38 Shares',
+    category: 'Green Activities',
+    participant_details: 'Sherlock Holmes, 221B Baker Street, London',
+    description: 'Youtube video',
+    thumbnail: '..',
+    value: '200'
+  },
+]
+
 
 
 export default function ActivitiesTab() {
@@ -8,6 +69,7 @@ export default function ActivitiesTab() {
         <div className="">
           <DialogUploadActivities/>
           <p>Table</p>
+          <Table data={data} headings={headings} />
         </div>
     )
 }
